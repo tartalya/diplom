d8b182c08ebabae5cadd9aafbedc6dc876cb4404
saha-work <brutal4028@gmail.com>
2017-10-05 12:54:01 +0300

    expect rewrite

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,39 +1,21 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array
      */
     protected $except = [
-        //
-        /*
-       '/login',
-       '/auth',
-       '/ask',
-       '/admin',
-       '/admin/users',
-       '/admin/users/{id}/edit',
-       '/admin/users/{id}',
-       '/admin/users/*',
-       '/admin/answer/add',
-       '/admin/answer',
-       '/admin/answer/manage',
-       '/admin/categories',
-       '/admin/answer/category',
-       '/admin/answer/hided'
-         * 
-         */
         '/admin/*',
         '/login',
         '/auth',
         '/ask'
-        
     ];
+
 }
